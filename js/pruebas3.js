24a68dc9f8f9cf32c5d7337b58292a64429429f1
Daniel Martínez <dmartinez@esliceu.com>
2024-01-26 20:06:49 +0100

    avance prácticas JS

--- js/pruebas3.js
+++ js/pruebas3.js
@@ -1,11 +1,8 @@
-
-
 function multiply(){
     var numero = document.getElementById("numero").value;
     var resultado = numero * 36;
     document.getElementById("resultado").value = resultado;
 }
-
 
 function muestraPasswd(){
     var elemento = document.getElementById("contraseña");
@@ -14,6 +11,42 @@ function muestraPasswd(){
         elemento.setAttribute("type", "text");
     } else if (elemento.getAttribute("type") == "text"){
         elemento.setAttribute("type", "password");
+    }
+
+}
+
+function modoNoche(){
+    var body = document.querySelector("body");
+
+    //Accediendo directamente al Style
+    // body.style.backgroundColor = "black";
+    // body.style.color = "white";
+
+    //Cambiando clases
+    // var list = body.classList;
+
+    // if (list.contains("modoDia")){
+    //     list.replace("modoDia", "modoNoche");
+    // } else if (list.contains("modoNoche")){
+    //     list.replace("modoNoche", "modoDia");
+    // }
+
+    //Cambiando directamente la hoja de estilos
+    var estilos = document.getElementById("estilos");
+    var botonModo = document.getElementById("botonModo");
+    var saludo = document.getElementById("saludo");
+
+    const HOJA_ESTILOS_DIA = "./css/modoDia.css";
+    const HOJA_ESTILOS_NOCHE = "./css/modoNoche.css";
+
+    if (estilos.getAttribute("href") == HOJA_ESTILOS_DIA){
+        estilos.setAttribute("href", HOJA_ESTILOS_NOCHE);
+        botonModo.value="☀️";
+        saludo.textContent = "Bona nit!";
+    } else if (estilos.getAttribute("href") == HOJA_ESTILOS_NOCHE){
+        estilos.setAttribute("href", HOJA_ESTILOS_DIA);
+        botonModo.value="🌙";
+        saludo.textContent = "Bon dia!";
     }
 
 }
